b06d878f32cf90049e04d387d446d89d7c160d7a
iris wang <rwang0399@gmail.com>
2024-02-01 12:03:50 +0800

    commit addfin1

--- jzg/test2.py
+++ jzg/test2.py
@@ -15,7 +15,8 @@ db = SQLAlchemy(app)
 
 class person(db.Model):
     __tablename__ = 'person'
-    personname = db.Column(db.String, primary_key=True)
+    personid = db.Column(db.String, primary_key=True)
+    personname = db.Column(db.String, nullable=True)
     gender = db.Column(db.String, nullable=True)
     orgindexcode = db.Column(db.String, nullable=True)
     birthday = db.Column(db.String, nullable=True)
@@ -25,6 +26,8 @@ class person(db.Model):
     certificateno = db.Column(db.String, nullable=True)
     jobno = db.Column(db.String, nullable=True)
     faces = db.Column(db.String, nullable=True)
+    facesid = db.Column(db.String, nullable=True)
+
 
 with app.app_context():
     db.create_all()
@@ -40,21 +43,11 @@ def sign(key, value):
     return base64.b64encode(temp.digest()).decode()
 
 def simulate_api_request():
-    print("simulate_api_request function called") 
+    existing_person = person.query.all()
     appKey = "22932116"
-    #print(auth_id)
-
-    # Example request data
-    request_data = {"name": "John Doe", "age": 30}
-    print("Authentication successful")
-
-    # Construct the signature string
-    sign_str = f"POST\n*/*\napplication/json\nx-ca-key:{appKey}\nx-ca-nonce:{x_ca_nonce}\nx-ca-timestamp:{x_ca_timestamp}\n{api_add_person_url}"
-
-    # Generate the signature
+    sign_str = f"POST\n*/*\napplication/json\nx-ca-key:{appKey}\nx-ca-nonce:{x_ca_nonce}\nx-ca-timestamp:{x_ca_timestamp}\n{api_add_person_url}"  
     signature = sign(appSecret, sign_str)
 
-    # Construct request headers
     headers = {
         "Accept": "*/*",
         'Content-Type': 'application/json',
@@ -64,39 +57,61 @@ def simulate_api_request():
         "x-ca-timestamp": x_ca_timestamp,
         "x-ca-nonce": x_ca_nonce
     }
-
-    # Send the request
-    return headers
-
-    
+    data = []
+    for person1 in existing_person:
+        data.append({
+        "personName": person1.personname,
+        "gender": person1.gender,
+        "orgIndexCode": person1.orgindexcode,
+        "birthday": person1.birthday,
+        "phoneNo": person1.phoneno,
+        "email": person1.email,
+        "certificateType": person1.certificatetype,
+        "certificateNo": person1.certificateno,
+        "jobNo": person1.jobno,
+        "faces": [
+            {
+                "faceData": person1.faces
+            }
+        ]
+    })
+    return headers, data
 
 @app.route(api_add_person_url, methods=['POST'])
 def simulate_request_route():
-    rheaders = simulate_api_request()
-    
-    xcakey = rheaders.get('x-ca-key')
-    xcasignatureheaders = request.headers.get('x-ca-signature-headers')
-    xcasignature = request.headers.get('x-ca-signature')
-    xcatimestamp = request.headers.get('x-ca-timestamp')
-    xcanonce = request.headers.get('x-ca-nonce')
+    existingperson = person.query.all()
+    headers, data = simulate_api_request()
+
+    xcakey = headers.get('x-ca-key')
+    xcasignatureheaders = headers.get('x-ca-signature-headers')
+    xcasignature = headers.get('x-ca-signature')
+    xcatimestamp = headers.get('x-ca-timestamp')
+    xcanonce = headers.get('x-ca-nonce')
 
     sign_str = f"POST\n*/*\napplication/json\nx-ca-key:{xcakey}\nx-ca-nonce:{xcanonce}\nx-ca-timestamp:{xcatimestamp}\n{api_add_person_url}"
 
-    print (xcakey)
-    if xcakey != "22932116":
-        return {"error": f"Request failed with status"}
-    return 1
+    if (
+        xcakey != "22932116" or
+        xcasignatureheaders != "x-ca-key,x-ca-nonce,x-ca-timestamp" or
+        xcasignature != sign(appSecret, sign_str) or
+        xcatimestamp != x_ca_timestamp or
+        xcanonce != x_ca_nonce
+    ):
+        return jsonify({"error": f"请求失败，状态不正确"})
 
-    # Check the status code
-    #if response1.status_code == 200:
-        # Parse the response and return the data
-    #    return response1.json()
-    #else:
-        # Handle the error status code
-    #    return {"error": f"Request failed with status code {response.status_code}"}
+    # Handle data separately
 
-
-    return jsonify(1)
-
+    result = []
+    for person2 in existingperson:
+        result.append({
+            "personId": person2.personid,
+            "faceId": person2.facesid,
+        })
+    return {
+    "code": "0",
+    "msg": "success",
+    "data": result
+}
+   
 if __name__ == '__main__':
     app.run(debug=True)
